e720c661e66223214bd2dacbc724fd15acb3b220
zas97 <joanc1997@gmail.com>
2017-12-13 23:40:21 +0100

    la

--- src/Client/ClientInterface.java
+++ src/Client/ClientInterface.java
@@ -1,16 +1,26 @@
 package Client;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
+/**
+ * Graphical User Interface
+ * <p>This class contains a graphical user interface that allows de user
+ * to control the chat in an easy and visual manner. It doesn't contain any of the logic that
+ * makes the chat work, it contains only a interface for the user to control the chat; all the logic is inside EchoClient.</p>
+ * <p>
+ * <p>In the top there is the username with a button to update it and the ip of the host server.</p>
+ * <p>
+ * <p>In the middle we have all the messages sent by the users or by other users.</p>
+ * <p>
+ * <p>In the bottom there is a text field that allows de users to write the message and to send it
+ * by clicking enter or by clicking on the button send</p>
+ */
 public class ClientInterface implements ActionListener {
     private JFrame frame;
     public JTextArea contentArea;
@@ -25,7 +35,15 @@ public class ClientInterface implements ActionListener {
     private EchoClient client;
     private final Color COLOR_CHAT = Color.BLUE;
 
-    public ClientInterface(EchoClient client,String host,String username) {
+    /**
+     * Constructor of ClientInterface
+     * it constructs all the graphical interface
+     *
+     * @param client   client connected to the chat
+     * @param host     ip of the host
+     * @param username username of the user using the chat
+     */
+    public ClientInterface(EchoClient client, String host, String username) {
         frame = new JFrame("Client");
         this.client = client;
 
@@ -50,10 +68,6 @@ public class ClientInterface implements ActionListener {
         btn_setUsername.addActionListener(this);
 
 
-
-
-
-
         southPanel = new JPanel(new BorderLayout());
         southPanel.setBorder(new TitledBorder("write"));
         southPanel.add(txt_message, "Center");
@@ -68,7 +82,6 @@ public class ClientInterface implements ActionListener {
         northPanel.add(txt_username);
         northPanel.add(new JLabel("IP host:"));
         northPanel.add(txt_host);
-
 
 
         frame.setLayout(new BorderLayout());
@@ -89,26 +102,36 @@ public class ClientInterface implements ActionListener {
 
     }
 
-    public void printMessage(String msg){
+    /**
+     * Prints a message in the content area so the user can read it followed by an endline
+     *
+     * @param msg message to be printed
+     */
+    public void printMessage(String msg) {
 
-        contentArea.append(msg +"\n");
+        contentArea.append(msg + "\n");
         contentArea.setCaretPosition(contentArea.getText().length() - 1);
 
     }
 
-
-
-
-    public void actionPerformed(ActionEvent e){
+    /**
+     * This method contains all the "reactions" of the chat to the user orders.
+     * It sends a message when the user wants to send a message.
+     * It changes the username when the user wants to change its username.
+     *
+     * @param e event that called actionPerformed
+     */
+    public void actionPerformed(ActionEvent e) {
         Object o = e.getSource();
-        if(o == btn_send || o==txt_message){
+        //send message
+        if (o == btn_send || o == txt_message) {
             client.SendMessage(txt_message.getText());
             txt_message.setText("");
-
-        }
-        else if(o==btn_setUsername || o==txt_username){
+        //change username
+        } else if (o == btn_setUsername || o == txt_username) {
             client.setName(txt_username.getText());
         }
+        //put the cursor in the txt_message field
         txt_message.requestFocus();
 
     }
--- src/Client/EchoClient.java
+++ src/Client/EchoClient.java
@@ -1,11 +1,19 @@
 package Client;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
+/**
+ * Client app for a chat system that allows to send message to all users using the multicast distribution implemented via
+ * sockets, each user has its own username.
+ * It also contains a graphical interface that allows to better control the chat
+ */
 public class EchoClient {
 
     private MulticastSocket socket;
@@ -14,60 +22,92 @@ public class EchoClient {
     private String username;
     private ClientInterface gui;
 
-    EchoClient(String address, int port,String username){
-        this.username=username;
+    /**
+     * Constructor of EchoClient
+     * Initializes the connection and the graphic user interface
+     *
+     * @param address  ip of the connection
+     * @param port     port of the connection
+     * @param username username of the user
+     */
+    EchoClient(String address, int port, String username) {
+        this.username = username;
         this.port = port;
         try {
             this.address = InetAddress.getByName(address);
             socket = new MulticastSocket(port);
             socket.joinGroup(this.address);
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
-        gui = new ClientInterface(this,address,username);
+        gui = new ClientInterface(this, address, username);
 
 
     }
 
-    public void startClient() throws IOException{
+    /**
+     * Checks constantly if something is written in the terminal and in that case it sends
+     * a message to all users connected.
+     * It stops everything when the user sends "."
+     *
+     * @throws IOException error reading the user input via the terminal
+     */
+    public void startClient() throws IOException {
         ReadServer r = new ReadServer();
         r.start();
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         String line;
 
         while (true) {
-            line=stdIn.readLine();
+            line = stdIn.readLine();
             if (line.equals(".")) break;
             SendMessage(line);
         }
     }
 
-    public void SendMessage(String msg){
+    /**
+     * sends a message to all user with the prefix of the actual time and the name
+     * of the user sending it
+     *
+     * @param msg message to be sent
+     */
+    public void SendMessage(String msg) {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         String time = sdf.format(new Date());
         try {
-            msg = time+" "+username + ": " + msg;
+            msg = time + " " + username + ": " + msg;
             DatagramPacket message = new DatagramPacket(msg.getBytes(), msg.length(),
                     address, port);
             socket.send(message);
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
     }
 
-    public void setName(String username){
+    /**
+     * changes the name of the user
+     *
+     * @param username new name for the user
+     */
+    public void setName(String username) {
         this.username = username;
     }
 
+    /**
+     * Thread that listens to messages sent by other users
+     */
     public class ReadServer extends Thread {
+        /**
+         * constantly checks if another user sent a message and in case that of recieving a message it
+         * prints it in the terminal and in the GUI.
+         */
         public void run() {
             while (true) {
                 try {
                     byte[] buf = new byte[10000];
                     DatagramPacket recv = new DatagramPacket(buf, buf.length);
                     socket.receive(recv);
-                    String msg = new String(recv.getData(),0,1024);
+                    String msg = new String(recv.getData(), 0, 1024);
                     System.out.println(msg);
                     gui.printMessage(msg);
                 } catch (IOException e) {
@@ -79,13 +119,17 @@ public class EchoClient {
     }
 
 
-
+    /**
+     * starts the client with a name selected or with the name Anonymous as default
+     *
+     * @param args ip_address, port, username
+     * @throws IOException error creating the sockets
+     */
     public static void main(String[] args) throws IOException {
 
 
-
         if (args.length != 3 && args.length != 2) {
-            System.out.println("Usage: java Client.EchoClient <Group address> <Group port> <username>");
+            System.out.println("Usage: java Client.EchoClient <Group address> <Group port> <Username>");
             System.out.println("or");
             System.out.println("Usage: java Client.EchoClient <Group address> <Group port>");
             System.exit(1);
@@ -94,15 +138,13 @@ public class EchoClient {
         String address = args[0];
         int port = Integer.valueOf(args[1]);
         String username;
-        if(args.length == 3){
-            username=args[2];
+        if (args.length == 3) {
+            username = args[2];
+        } else {
+            username = "Anonymous";
         }
-        else{
-            username="Anonymous";
-        }
-        EchoClient c = new EchoClient(address,port,username);
+        EchoClient c = new EchoClient(address, port, username);
         c.startClient();
-
 
 
     }
